6a74a5ff418a2530a8eb50817bdc425e9c320b7d
Aravind <balajiaravind@gmail.com>
2026-05-30 00:34:58 +0530

    update

--- kafka-producer/app.js
+++ kafka-producer/app.js
@@ -17,7 +17,6 @@ const connectProducer = async () => {
 };
 connectProducer();
 
-console.log('Check Log ----> 1')
 app.get("/count", async (req, res) => {
   const offsets = await admin.fetchTopicOffsets("click-events");
 
